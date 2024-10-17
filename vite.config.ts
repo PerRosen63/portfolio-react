b4390c26a18907ad8a0dbcd3a48a06b547e416f0
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-10-18 00:40:25 +0200

    🚀 base again

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === "production" ? "/portfolio-react/" : "/", // Set base for production
+    define: {
+      "process.env.BASE_PATH": JSON.stringify(
+        mode === "production" ? "/portfolio-react/" : "/"
+      ),
+    },
+  };
 });
