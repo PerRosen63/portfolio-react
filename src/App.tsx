24ea40506bf9b04bee2849d00217482f4f9c4c46
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-12-10 10:20:10 +0100

    ✨ scroll down arrow

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import { ChevronDown } from "lucide-react";
 import PostsList from "./components/PostsList";
 import { Profile } from "./components/Profile";
 
 function App() {
+  const onClick = () => {
+    window.location.href = "#tabs";
+  };
+
   return (
     <>
-      <main className="max-lg:container max-w-6xl md:px-4 mx-auto">
+      <main className="max-lg:container max-w-6xl md:px-4 mx-auto scroll-smooth">
         <section className="flex-col max-w-full mx-auto flex items-center justify-between gap-4 p-4">
           <Profile></Profile>
+          <button className="max-lg:hidden my-0.5 hover:my-0">
+            {" "}
+            <ChevronDown
+              className="h-8 w-8 shrink-0 text-muted-foreground hover:text-foreground hover:h-9 hover:w-9"
+              onClick={onClick}
+            ></ChevronDown>
+          </button>
         </section>
-        <section className="max-w-full mx-auto p-4">
+        <section id="tabs" className="max-w-full mx-auto p-4">
           <PostsList></PostsList>
         </section>
       </main>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,6 +2,8 @@ import parse from "html-react-parser";
 import page from "../data/page.json";
 import { Page } from "../types/Page";
 import { FC } from "react";
+import { Mail, Phone } from "lucide-react";
+import { Github, Linkedin } from "grommet-icons";
 
 export const Profile: FC = () => {
   const pageData: Page = page;
@@ -23,12 +25,12 @@ export const Profile: FC = () => {
 
   return (
     <>
-      <div className="flex flex-col my-12 flex-grow flex-wrap content-center">
+      <div className="flex flex-col max-lg:my-12 flex-grow flex-wrap content-center justify-center lg:h-[15vh]">
         <h1 className="text-center">
           Per Rosén <span className="max-md:hidden">-</span> cv/portfolio
         </h1>
       </div>
-      <div className="md:grid gap-12 md:grid-cols-3">
+      <div className="md:grid gap-12 md:grid-cols-3 lg:h-[75vh]">
         <div className="md:order-1 ">
           {image && (
             <img
@@ -44,6 +46,14 @@ export const Profile: FC = () => {
           <p className="[&>h2]:mb-4 [&>h3]:text-base [&>h3]:font-semibold [&>h3]:uppercase [&>h3]:mt-2 [&>h3]:font-[Roboto]">
             {parse(pageData.content)}{" "}
           </p>
+          <div className="my-4">
+            <a className="mr-4" href="https://github.com/PerRosen63">
+              <Github color="plain" size="3rem" />
+            </a>
+            <a href="https://www.linkedin.com/in/per-ros%C3%A9n-a27a5060/">
+              <Linkedin color="plain" size="3rem" />
+            </a>
+          </div>
         </div>
 
         <div className="md:order-2 bg-muted max-md:mt-4 p-4 rounded-lg border">
@@ -56,6 +66,16 @@ export const Profile: FC = () => {
             <li>född och uppvuxen i Nynäshamn</li>
             <li>intresserad av odling och mat</li>
           </ul>
+          <div className="my-2">
+            <a className="flex" href="mailto:per@ronz.org">
+              <Mail aria-label="mail-icon" />
+              <span className="mx-1">per@ronz.org</span>
+            </a>
+          </div>
+          <div className="my-2 flex">
+            <Phone aria-label="phone-icon" className="-scale-x-100" />
+            <span className="mx-1">0731-515063</span>
+          </div>
         </div>
       </div>
     </>
